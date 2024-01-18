57861ab557458b55b23583e715819aa19ed12124
Jeff Bridgforth <jeff.bridgforth@gmail.com>
2024-01-17 19:57:59 -0500

    Evolution 1
    - Added theme switcher
    - Desktop Nav underneath header
    - Added social links

--- header.php
+++ header.php
@@ -16,4 +16,37 @@
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?family=Lora:wght@500;700&family=Nunito:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
         <?php wp_head(); ?>
+
+		<script>
+			(function () {
+				const Theme = { AUTO: 'auto', LIGHT: 'light', DARK: 'dark' };
+				const THEME_STORAGE_KEY = 'theme';
+				const THEME_OWNER = document.documentElement;
+
+				const cachedTheme = localStorage.getItem(THEME_STORAGE_KEY);
+				if (cachedTheme) {
+				THEME_OWNER.dataset[THEME_STORAGE_KEY] = cachedTheme;
+				}
+
+				document.addEventListener('DOMContentLoaded', () => {
+				const themePicker = document.getElementById('theme-picker');
+				if (!themePicker) return;
+
+				themePicker.addEventListener('change', (e) => {
+					const theme = e.target.value;
+					if (theme === Theme.AUTO) {
+					delete THEME_OWNER.dataset[THEME_STORAGE_KEY];
+					localStorage.removeItem(THEME_STORAGE_KEY);
+					} else {
+					THEME_OWNER.dataset[THEME_STORAGE_KEY] = theme;
+					localStorage.setItem(THEME_STORAGE_KEY, theme);
+					}
+				});
+
+				const initialTheme = cachedTheme ?? Theme.AUTO;
+				themePicker.querySelector('input[checked]').removeAttribute('checked');
+				themePicker.querySelector(`input[value="${initialTheme}"]`).setAttribute('checked', '');
+				});
+			})();
+		</script>
 </head>
--- includes/branding.php
+++ includes/branding.php
@@ -12,8 +12,68 @@
 				</div>
 			</a>
 		</header>
-		<nav id="nav_main" class="nav-main">
-			<?php wp_nav_menu( array( 'theme_location' => 'banner-nav','container' => 'false' )); ?>
-		</nav>
+		<div class="utilities">
+			<ul class="social-links">
+				<li>
+					<a href="https://mastodon.social/@webcraftsman">
+						<svg xmlns="http://www.w3.org/2000/svg" height="1em" fill="currentColor" viewBox="0 0 419 453">
+							<path d="M 419 149 Q 418 101 402 73 L 402 73 Q 387 46 372 34 L 372 34 Q 356 23 355 23 L 355 23 Q 355 23 355 23 Q 322 9 266 4 Q 210 0 154 4 Q 98 9 65 23 Q 64 23 48 34 Q 33 46 18 73 Q 2 101 1 149 Q 1 158 1 167 Q 0 225 5 281 Q 9 338 32 380 Q 55 423 107 438 Q 168 453 210 450 Q 248 447 269 440 Q 289 432 289 432 Q 289 432 289 432 L 288 395 Q 287 395 264 400 Q 242 405 211 405 Q 179 405 153 396 Q 127 387 121 351 Q 120 344 120 337 Q 181 350 227 349 Q 274 348 294 344 Q 297 344 299 344 Q 342 338 373 316 Q 404 295 410 271 Q 417 230 418 191 Q 419 152 419 149 L 419 149 Z M 344 274 L 297 274 L 297 160 Q 296 135 281 127 Q 265 118 250 128 Q 234 138 233 167 L 233 230 L 187 230 L 187 167 Q 186 138 170 128 Q 155 118 140 127 Q 124 135 123 160 L 123 274 L 76 274 Q 76 243 76 219 Q 75 164 78 139 Q 81 114 95 99 Q 116 77 147 78 Q 179 78 198 105 L 210 125 L 222 105 Q 242 78 273 78 Q 304 77 325 99 Q 340 114 342 139 Q 345 164 344 219 Q 344 242 344 274 L 344 274 Z" />
+						</svg>
+						<span class="text sr-only">Mastodon</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://www.linkedin.com/in/jeffbridgforth/">
+						<svg xmlns="http://www.w3.org/2000/svg" height="1em" fill="currentColor" viewBox="0 0 448 448">
+							<path
+								d="M 416 0 L 32 0 Q 18 0 9 9 Q 0 19 0 32 L 0 416 Q 0 429 9 439 Q 18 448 32 448 L 416 448 Q 430 448 439 439 Q 448 429 448 416 L 448 32 Q 448 19 439 9 Q 430 0 416 0 L 416 0 Z M 135 384 L 69 384 L 69 170 L 136 170 L 136 384 L 135 384 Z M 102 141 Q 86 141 75 130 L 75 130 Q 64 119 64 103 Q 64 86 75 75 Q 86 64 102 64 Q 119 64 129 75 Q 140 86 141 103 Q 140 119 129 130 Q 119 141 102 141 L 102 141 Z M 384 384 L 318 384 L 318 280 Q 319 260 313 242 Q 308 225 283 223 Q 258 224 250 240 Q 243 256 244 278 L 244 384 L 177 384 L 177 170 L 241 170 L 241 199 L 242 199 Q 248 186 264 176 Q 280 165 305 165 Q 354 166 370 194 Q 386 223 384 267 L 384 384 L 384 384 Z"
+							/>
+						</svg>
+						<span class="text sr-only">LinkedIn</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://codepen.io/webcraftsman">
+						<svg xmlns="http://www.w3.org/2000/svg" height="1em" fill="currentColor" viewBox="0 0 512 520">
+							<path
+								d="M 502 164 L 268 8 Q 256 0 244 8 L 10 164 Q 0 170 0 182 L 0 338 Q 0 349 10 356 L 244 512 Q 256 520 268 512 L 502 356 Q 512 349 512 338 L 512 182 Q 512 170 502 164 L 502 164 Z M 278 67 L 450 182 L 373 233 L 278 170 L 278 67 L 278 67 Z M 234 67 L 234 170 L 139 233 L 62 182 L 234 67 L 234 67 Z M 44 223 L 99 260 L 44 297 L 44 223 L 44 223 Z M 234 453 L 62 338 L 139 287 L 234 350 L 234 453 L 234 453 Z M 256 312 L 178 260 L 256 208 L 334 260 L 256 312 L 256 312 Z M 278 453 L 278 350 L 373 287 L 450 338 L 278 453 L 278 453 Z M 468 297 L 413 260 L 468 223 L 468 297 L 468 297 Z"
+							/>
+						</svg>
+						<span class="text sr-only">Codepen</span>
+					</a>
+				</li>
+				<li>
+					<a href="/feed/">
+						<svg xmlns="http://www.w3.org/2000/svg" height="1em" fill="currentColor" viewBox="0 0 448 457">
+							<path
+								d="M 0 32 Q 0 18 9 9 L 9 9 Q 18 0 32 0 Q 148 1 242 57 Q 336 112 391 206 Q 447 300 448 416 Q 448 430 439 439 Q 430 448 416 448 Q 402 448 393 439 Q 384 430 384 416 Q 383 318 336 238 Q 289 159 210 112 Q 130 65 32 64 Q 18 64 9 55 Q 0 46 0 32 L 0 32 Z M 0 384 Q 1 348 32 329 Q 64 311 96 329 Q 127 348 128 384 Q 127 420 96 439 Q 64 457 32 439 Q 1 420 0 384 L 0 384 Z M 32 128 Q 112 129 177 167 L 177 167 Q 242 206 281 271 L 281 271 Q 319 336 320 416 Q 320 430 311 439 Q 302 448 288 448 Q 274 448 265 439 Q 256 430 256 416 Q 255 353 225 303 Q 196 252 145 223 Q 95 193 32 192 Q 18 192 9 183 Q 0 174 0 160 Q 0 146 9 137 Q 18 128 32 128 L 32 128 Z"
+							/>
+						</svg>
+						<span class="text sr-only">Feed</span>
+					</a>
+				</li>
+			</ul>
+			<fieldset id="theme-picker" class="theme-picker">
+				<legend class="sr-only">Theme:</legend>
+				<span class="visible-label" aria-hidden="true">Theme:</span>
+				<label>
+					<input name="theme" type="radio" value="auto" checked>
+					Auto
+				</label>
+				<label>
+					<input name="theme" type="radio" value="light">
+					Light
+				</label>
+				<label>
+					<input name="theme" type="radio" value="dark">
+					Dark
+				</label>
+			</fieldset>
+
+		</div>
 		<button id="mobile-nav" class="mobile-nav">Menu</a>
 </header>
+
+<nav id="nav_main" class="nav-main">
+	<?php wp_nav_menu( array( 'theme_location' => 'banner-nav','container' => 'false' )); ?>
+</nav>
